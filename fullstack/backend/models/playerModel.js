8426a3e5694cff2439ec4f3d0d0c62d33eb8b841
Yemi <yajibola23gmail.com>
2025-05-16 22:21:52 -0500

    Created player models to handl sql queries.

---
+++ fullstack/backend/models/playerModel.js
@@ -0,0 +1,24 @@
+const db = require("../db");
+
+function getAllPlayers() {
+  const stmt = db.prepare("SELECT * FROM players");
+  return stmt.all();
+}
+
+function addNewPlayer(name, points) {
+  const stmt = db.prepare("INSERT INTO players (name, points) VALUES(?, ?)");
+  const result = stmt.run(name, points);
+
+  return {
+    id: result.lastInsertRowid,
+    name,
+    points,
+  };
+}
+
+function deletePlayer(id) {
+  const stmt = db.prepare("DELETE FROM players WHERE id = ?");
+  const result = stmt.run(id);
+
+  return result.changes > 0;
+}
