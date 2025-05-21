2bb5a1ee0a1f8d12c3eea1d0e57043964e20a541
Yemi <yajibola23gmail.com>
2025-05-21 18:03:27 -0500

    Add auth routes for signup and login

--- fullstack/backend/app.js
+++ fullstack/backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const playerRoutes = require('./routes/players');
+const authRoutes = require('./routes/auth')
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Mount your routes
-app.use('/api/players', playerRoutes);
+app.use('/api/players', playerRoutes)
+app.use('/api', authRoutes)
 
 module.exports = app;
