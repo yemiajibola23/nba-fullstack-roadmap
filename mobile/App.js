c86a283f62f2118ab7e98343f6a633390d53f010
Yemi <yajibola23gmail.com>
2025-05-28 16:45:21 -0500

    feat{day-35}: display backend player list in mobile app with FlatList

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import AppNavigator from "./navigation/AppNavigator";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppNavigator />
+    </GestureHandlerRootView>
+  );
 }
 
 const styles = StyleSheet.create({
--- mobile/screens/HomeScreen.js
+++ mobile/screens/HomeScreen.js
@@ -1,21 +1,60 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList } from "react-native";
 
 export default function HomeScreen() {
+  const [players, setPlayers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/players")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Fetched players:", data);
+        setPlayers(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching players:", err);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size={"large"} color={"#555"} />
+        <Text>Loading players...</Text>
+      </View>
+    );
+  }
+
+  console.log("Players in render:", players );
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>🏀 Welcome to NBA Mobile App</Text>
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.points}>Points: {item.points}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  center: { flex: 1, justifyContent: "center", alignItems: "center" },
+  card: {
+    backgroundColor: "#f4f4f4",
+    padding: 16,
+    marginBottom: 12,
+    borderRadius: 16,
   },
-  text: {
-    fontSize: 18,
-  },
+  name: { fontSize: 18, fontWeight: "bold" },
+  points: { fontSize: 16, color: "#333" },
 });
